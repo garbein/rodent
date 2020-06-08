b4cadec965489f91c6cfc8c2995542a28b358029
yinchuanwen <yinchuanwen@chelun.com>
2020-06-08 14:58:24 +0800

    modify sms config

--- src/utils/sms.rs
+++ src/utils/sms.rs
@@ -5,8 +5,8 @@ use percent_encoding::{utf8_percent_encode, AsciiSet, CONTROLS};
 use crate::utils::crypt;
 
 pub async fn send_sms(phone:&str, content:&str) -> Result<(),reqwest::Error> {
-    let sms_account = "copoor";
-    let sms_token = crypt::md5("Sdtx20180418");
+    let sms_account = std::env::var("SMS_ACCOUT").unwrap();
+    let sms_token = crypt::md5(std::env::var("SMS_PASSWORD").unwrap());
     let smsapi = "http://api.smsbao.com/sms"; //短信网关
 
     const QUERY: &AsciiSet = &CONTROLS.add(b' ').add(b'"').add(b'#').add(b'<').add(b'>');
